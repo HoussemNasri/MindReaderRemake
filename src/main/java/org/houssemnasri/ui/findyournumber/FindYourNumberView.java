823ab6c9b14329919ee59c4a42d8e52ff52629ea
Houssem Nasri <housi.housi2015@gmail.com>
2020-12-02 13:51:58 +0100

    Add FindYourNumberView

---
+++ src/main/java/org/houssemnasri/ui/findyournumber/FindYourNumberView.java
@@ -0,0 +1,15 @@
+package org.houssemnasri.ui.findyournumber;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+import de.saxsys.mvvmfx.FxmlView;
+
+public class FindYourNumberView implements FxmlView<FindYourNumberViewModel>, Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
---
+++ src/main/java/org/houssemnasri/ui/findyournumber/FindYourNumberViewModel.java
@@ -0,0 +1,6 @@
+package org.houssemnasri.ui.findyournumber;
+
+import de.saxsys.mvvmfx.ViewModel;
+
+public class FindYourNumberViewModel implements ViewModel {
+}
